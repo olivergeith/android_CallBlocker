c7d5eeff5ab81d8c988a0c76244c58b194641ed5
olivergeith <oliver.geith@gmx.net>
2017-11-22 12:12:09 +0100

    Allow Wildcard

--- src/com/androidbegin/callblocker/AddToBlocklistActivity.java
+++ src/com/androidbegin/callblocker/AddToBlocklistActivity.java
@@ -1,3 +1,4 @@
+
 package com.androidbegin.callblocker;
 
 import android.app.Activity;
@@ -11,108 +12,97 @@ import android.widget.EditText;
 
 public class AddToBlocklistActivity extends Activity implements OnClickListener {
 
-	// Declaration all on screen components
-	private EditText country_code_et, phone_et;
-	private Button reset_btn, submit_btn;
-	
-	// Declaration of BlacklistDAO to interact with SQlite database
-	private BlacklistDAO blackListDao;
+    // Declaration all on screen components
+    private EditText country_code_et, phone_et;
+    private Button reset_btn, submit_btn;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_add_to_blocklist);
-		
-		// Initialization of the DAO object.
-		blackListDao = new BlacklistDAO(this);
+    // Declaration of BlacklistDAO to interact with SQlite database
+    private BlacklistDAO blackListDao;
 
-		country_code_et = (EditText) findViewById(R.id.country_code_et);
-		phone_et = (EditText) findViewById(R.id.phone_et);
-		reset_btn = (Button) findViewById(R.id.reset_btn);
-		submit_btn = (Button) findViewById(R.id.submit_btn);
-		
-		reset_btn.setOnClickListener(this);
-		submit_btn.setOnClickListener(this);
-	}
-	
-	@Override
-	public void onClick(View v) {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_to_blocklist);
 
-		if(v == submit_btn)
-		{
-			// All input fields are mandatory, so made a check
-			if(country_code_et.getText().toString().trim().length() > 0 && 
-					phone_et.getText().toString().trim().length() > 0)
-			{
-				// Once click on "Submit", it's first creates the Blacklist object 
-				final Blacklist phone = new Blacklist();
-				
-				// Then, set all the values from user input
-				phone.phoneNumber = "+" + country_code_et.getText().toString() + phone_et.getText().toString();
-				
-				// Insert the object to the database
-				blackListDao.create(phone);
-				
-				// Show the success message to user
-				showDialog();
-			}
-			// Show a dialog with appropriate message in case input fields are blank
-			else
-			{
-				showMessageDialog("All fields are mandatory !!");
-			}
-		}
-		else if(v == reset_btn)
-		{
-			reset();
-		}
-	}
-	
-	// Clear the entered text
-	private void reset()
-	{
-		country_code_et.setText("");
-		phone_et.setText("");
-	}
-	
-	private void showDialog()
-	{
-		// After submission, Dialog opens up with "Success" message. So, build the AlartBox first
-		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		
-		// Set the appropriate message into it.
-		alertDialogBuilder.setMessage("Phone Number added to block list successfully !!");
-		
-		// Add a positive button and it's action. In our case action would be, just hide the dialog box , 
-		// and erase the user inputs.
-		alertDialogBuilder.setPositiveButton("Add More",
-				new DialogInterface.OnClickListener() {
+        // Initialization of the DAO object.
+        blackListDao = new BlacklistDAO(this);
 
-					@Override
-					public void onClick(DialogInterface arg0, int arg1) {
-						reset();
-					}
-				});
-		
-		// Add a negative button and it's action. In our case, close the current screen
-		alertDialogBuilder.setNegativeButton("Cancel",
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						finish();
-					}
-				});
+        country_code_et = (EditText) findViewById(R.id.country_code_et);
+        phone_et = (EditText) findViewById(R.id.phone_et);
+        reset_btn = (Button) findViewById(R.id.reset_btn);
+        submit_btn = (Button) findViewById(R.id.submit_btn);
 
-		// Now, create the Dialog and show it.
-		final AlertDialog alertDialog = alertDialogBuilder.create();
-		alertDialog.show();
-	}
-	
-	private void showMessageDialog(final String message)
-	{
-		final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
-		alertDialogBuilder.setMessage(message);
-		final AlertDialog alertDialog = alertDialogBuilder.create();
-		alertDialog.show();
-	}
+        reset_btn.setOnClickListener(this);
+        submit_btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View v) {
+
+        if (v == submit_btn) {
+            // All input fields are mandatory, so made a check
+            if (country_code_et.getText().toString().trim().length() > 0) {
+                // Once click on "Submit", it's first creates the Blacklist object
+                final Blacklist phone = new Blacklist();
+
+                // Then, set all the values from user input
+                phone.phoneNumber = "+" + country_code_et.getText().toString() + phone_et.getText().toString();
+
+                // Insert the object to the database
+                blackListDao.create(phone);
+
+                // Show the success message to user
+                showDialog();
+            }
+            // Show a dialog with appropriate message in case input fields are blank
+            else {
+                showMessageDialog("All fields are mandatory !!");
+            }
+        } else if (v == reset_btn) {
+            reset();
+        }
+    }
+
+    // Clear the entered text
+    private void reset() {
+        country_code_et.setText("");
+        phone_et.setText("");
+    }
+
+    private void showDialog() {
+        // After submission, Dialog opens up with "Success" message. So, build the AlartBox first
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+
+        // Set the appropriate message into it.
+        alertDialogBuilder.setMessage("Phone Number added to block list successfully !!");
+
+        // Add a positive button and it's action. In our case action would be, just hide the dialog box ,
+        // and erase the user inputs.
+        alertDialogBuilder.setPositiveButton("Add More", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(final DialogInterface arg0, final int arg1) {
+                reset();
+            }
+        });
+
+        // Add a negative button and it's action. In our case, close the current screen
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(final DialogInterface dialog, final int which) {
+                finish();
+            }
+        });
+
+        // Now, create the Dialog and show it.
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+    private void showMessageDialog(final String message) {
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
 }
--- src/com/androidbegin/callblocker/CallBarring.java
+++ src/com/androidbegin/callblocker/CallBarring.java
@@ -1,62 +1,59 @@
+
 package com.androidbegin.callblocker;
 
 import java.lang.reflect.Method;
+
+import com.android.internal.telephony.ITelephony;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.telephony.TelephonyManager;
 
-import com.android.internal.telephony.ITelephony;
-
 // Extend the class from BroadcastReceiver to listen when there is a incoming call
-public class CallBarring extends BroadcastReceiver
-{
-	// This String will hold the incoming phone number
-	private String number;
-	
-	@Override
-	public void onReceive(Context context, Intent intent) 
-	{
-		// If, the received action is not a type of "Phone_State", ignore it
-	     if (!intent.getAction().equals("android.intent.action.PHONE_STATE")) 
-	         return;
-	     
-	    // Else, try to do some action
-	     else 
-	     {
-	    	 // Fetch the number of incoming call
-	         number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
-	         
-	         // Check, whether this is a member of "Black listed" phone numbers stored in the database
-	         if(MainActivity.blockList.contains(new Blacklist(number)))
-	         {
-	        	 // If yes, invoke the method
-	        	 disconnectPhoneItelephony(context);
-	             return;
-	         }
-	     }  
-	 }
+public class CallBarring extends BroadcastReceiver {
+    // This String will hold the incoming phone number
+    private String number;
 
-	// Method to disconnect phone automatically and programmatically 
-	// Keep this method as it is 
-	 @SuppressWarnings({ "rawtypes", "unchecked" })
-	 private void disconnectPhoneItelephony(Context context) 
-	 {
-		 ITelephony telephonyService;
-		 TelephonyManager telephony = (TelephonyManager) 
-		 context.getSystemService(Context.TELEPHONY_SERVICE);  
-		 try 
-		 {
-			 Class c = Class.forName(telephony.getClass().getName());
-			 Method m = c.getDeclaredMethod("getITelephony");
-			 m.setAccessible(true);
-			 telephonyService = (ITelephony) m.invoke(telephony);
-		     telephonyService.endCall();
-		 } 
-		 catch (Exception e) 
-		 {
-			 e.printStackTrace();
-		 }
-	 }
+    @Override
+    public void onReceive(final Context context, final Intent intent) {
+        // If, the received action is not a type of "Phone_State", ignore it
+        if (!intent.getAction().equals("android.intent.action.PHONE_STATE")) {
+            return;
+        } else {
+            // Fetch the number of incoming call
+            number = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER);
+
+            // Check, whether this is a member of "Black listed" phone numbers stored in the database
+            for (final Blacklist bl : MainActivity.blockList) {
+                if (number.startsWith(bl.phoneNumber)) {
+                    // If yes, invoke the method
+                    disconnectPhoneItelephony(context);
+                    return;
+                }
+            }
+            // if (MainActivity.blockList.contains(new Blacklist(number))) {
+            // // If yes, invoke the method
+            // disconnectPhoneItelephony(context);
+            // return;
+            // }
+        }
+    }
+
+    // Method to disconnect phone automatically and programmatically
+    // Keep this method as it is
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void disconnectPhoneItelephony(final Context context) {
+        ITelephony telephonyService;
+        final TelephonyManager telephony = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        try {
+            final Class c = Class.forName(telephony.getClass().getName());
+            final Method m = c.getDeclaredMethod("getITelephony");
+            m.setAccessible(true);
+            telephonyService = (ITelephony) m.invoke(telephony);
+            telephonyService.endCall();
+        } catch (final Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
